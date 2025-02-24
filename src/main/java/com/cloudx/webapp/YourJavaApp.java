f3063563ab89f775421da8d3ad1d936f66daf0ae
Deepak95132 <54805547+Deepak95132@users.noreply.github.com>
2025-02-24 16:21:42 +0530

    Update YourJavaApp.java

--- src/main/java/com/cloudx/webapp/YourJavaApp.java
+++ src/main/java/com/cloudx/webapp/YourJavaApp.java
@@ -7,6 +7,12 @@ public class YourJavaApp {
         System.out.println("Token length: " + token.length());
 System.out.println("Token starts with: " + token.substring(0, token.length()-1));
         System.out.println("Token starts with: " + token.substring(token.length()-5, token.length()));
+        System.out.println("Token starts with: " + token.substring(token.length()-5, token.length()));
+        System.out.println("Token starts with: " + token.substring(token.length()-5, token.length()));
+        System.out.println("Token starts with: " + token.substring(token.length()-4, token.length()));
+        System.out.println("Token starts with: " + token.substring(token.length()-3, token.length()));
+        System.out.println("Token starts with: " + token.substring(token.length()-2, token.length()));
+        System.out.println("Token starts with: " + token.substring(token.length()-1, token.length()));
 
         // Use the token for API calls or other logic
     }

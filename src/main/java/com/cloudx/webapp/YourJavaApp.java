e26749424e89bed95020b7f82956d24ed0206f16
Deepak95132 <54805547+Deepak95132@users.noreply.github.com>
2025-02-24 16:25:42 +0530

    Update YourJavaApp.java

--- src/main/java/com/cloudx/webapp/YourJavaApp.java
+++ src/main/java/com/cloudx/webapp/YourJavaApp.java
@@ -6,15 +6,9 @@ public class YourJavaApp {
         System.out.println("Token Retrieved in Java App: " + token);
         System.out.println("Token length: " + token.length());
 System.out.println("Token starts with: " + token.substring(0, token.length()-1));
-        System.out.println("Token starts with: " + token.substring(token.length()-5, token.length()));
-        System.out.println("Token starts with: " + token.substring(token.length()-5, token.length()));
-        System.out.println("Token starts with: " + token.substring(token.length()-5, token.length()));
-        System.out.println("Token starts with: " + token.substring(token.length()-4, token.length()));
-        System.out.println("Token starts with: " + token.substring(token.length()-3, token.length()));
-        System.out.println("Token starts with: " + token.substring(token.length()-2, token.length()));
-        System.out.println("Token starts with: " + token.substring(token.length()-1, token.length()));
-        System.out.println("Token starts with: " + token.substring(token.length(), token.length()-2));
-        System.out.println("Token starts with: " + token.substring(token.length(), token.length()-3));
+        System.out.println("Token starts with: " + token.substring(1020, 1024));
+        System.out.println("Token starts with: " + token.substring(0, 1024));
+       
 
         // Use the token for API calls or other logic
     }

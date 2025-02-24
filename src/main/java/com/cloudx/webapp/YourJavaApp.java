df13be15cd87c66593ce3251b7bff0fc55342a82
Deepak95132 <54805547+Deepak95132@users.noreply.github.com>
2025-02-24 16:14:33 +0530

    Update YourJavaApp.java

--- src/main/java/com/cloudx/webapp/YourJavaApp.java
+++ src/main/java/com/cloudx/webapp/YourJavaApp.java
@@ -4,6 +4,8 @@ public class YourJavaApp {
     public static void main(String[] args) {
         String token = System.getenv("OIDC_TOKEN");
         System.out.println("Token Retrieved in Java App: " + token);
+        System.out.println("Token length: " + token.length());
+System.out.println("Token starts with: " + token.substring(0, 5));
 
         // Use the token for API calls or other logic
     }

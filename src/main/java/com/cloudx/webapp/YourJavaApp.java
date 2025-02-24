1ae359e473a422c30003ef2fada892f5654214b5
Deepak95132 <54805547+Deepak95132@users.noreply.github.com>
2025-02-24 15:43:12 +0530

    Create YourJavaApp.java

---
+++ src/main/java/com/cloudx/webapp/YourJavaApp.java
@@ -0,0 +1,8 @@
+public class YourJavaApp {
+    public static void main(String[] args) {
+        String token = System.getenv("OIDC_TOKEN");
+        System.out.println("Token Retrieved in Java App: " + token);
+
+        // Use the token for API calls or other logic
+    }
+}

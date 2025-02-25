212021c72032732f9c467e53e159e3ca12a5467d
Deepak95132 <54805547+Deepak95132@users.noreply.github.com>
2025-02-25 11:46:59 +0530

    Update YourJavaApp.java

--- src/main/java/com/cloudx/webapp/YourJavaApp.java
+++ src/main/java/com/cloudx/webapp/YourJavaApp.java
@@ -5,7 +5,7 @@ public class YourJavaApp {
         String token = System.getenv("OIDC_TOKEN");
         System.out.println("Token Retrieved in Java App: " + token);
         System.out.println("Token length: " + token.length());
-System.out.println("Token starts with: " + token.substring(0, token.length()-1));
+        System.out.println("Token starts with: " + token.substring(0, token.length()-1));
         System.out.println("Token starts with: " + token.substring(1020, 1024));
         System.out.println("Token starts with: " + token.substring(1, 1024));
        
